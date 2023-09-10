4fcb7c188a717175f640d4b7b18562d8e2950cb1
MundiaNderi <sarahngima77@gmail.com>
2023-09-10 13:18:59 +0300

    Fixing semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 
-
 const request = require('request');
 const argv = require('process').argv;
 
 request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (err, res, body) {
-    if (err) throw err;
-    const actors = JSON.parse(body).characters;
-    extractOrder(actors, 0);
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  extractOrder(actors, 0);
 });
 
 const extractOrder = (actors, x) => {
-    if(x === actors.length) return;
-    request(actors[x], function (err, res, body) {
-        if (err) throw err;
-        console.log(JSON.parse(body).name);
-        extractOrder(actors, x + 1);
-    });
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    extractOrder(actors, x + 1);
+  });
 };
