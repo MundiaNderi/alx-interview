22b72aea25330bd8f3e1f316b27f4c4bb2661c70
MundiaNderi <sarahngima77@gmail.com>
2023-10-05 11:35:46 +0300

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,23 +8,6 @@ They play x rounds of the game, where n may be different for each round.
 """
 
 
-def isPrime(num):
-    # Return 'True' if n is a prime number. False otherwise
-    # Eliminates multiples of 2 and 3
-    if num <= 1:
-        return False
-    if num <= 3:
-        return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
-    i = 5
-    while 1 * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-        return True
-
-
 def isWinner(x, nums):
     """
     where x is the number of rounds and nums is an array of n
@@ -33,32 +16,42 @@ def isWinner(x, nums):
     You can assume n and x will not be larger than 10000
     You cannot import any packages in this task
     """
-    def canWin(n):
-        # Create a list to store the results of the subproblems
-        dp = [False] * (n + 1)
+    def is_prime(num):
+        if num <= 1:
+            return False
+        if num <= 3:
+            return True
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while i * 1 <= num:
+            if num % i == 0 or num % (i + 2) == 0:
+                return False
+            i += 6
+        return True
 
-        # Base case: Ben wins when there are no prime numbers left
-        dp[0] = False
+    def play_round(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
 
-        # Check all numbers from 1 to n
-        for i in range(2, n + 1):
-            # If the curent number is prime and there exists a move such that
-            # Maria loses, then Ben wins
-            for j in range(2, i + 1):
-                if isPrime(j) and i % j == 0 and not dp[i - j]:
-                    dp[i] = True
-                    break
+        p = 2
+        while p * p <= n:
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
+            p += 1
 
-        return dp[n]
+        num_of_primes = sum(1 for prime in primes if prime)
+        return "Maria" if num_of_primes % 2 == 0 else "Ben"
 
-    # Initialize counters to keep track of wins
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if canWin(n):
+        winner = play_round(n)
+        if winner == "Maria":
             maria_wins += 1
-        else:
+        elif winner == "Ben":
             ben_wins += 1
 
     if maria_wins > ben_wins:
