19980ff335e81398d4f3d81f7944156c3dbd16b5
MundiaNderi <sarahngima77@gmail.com>
2023-10-05 11:42:11 +0300

    Reverting to previous changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-"""
-Given a set of consecutive integers starting from 1 up to and including n,
-they take turns choosing a prime number from the set and removing that
-number and its multiples from the set. The player that
-cannot make a move loses the game.
+"""Given a set of consecutive integers starting from 1 up to
+and including n,they take turns choosing a prime number from the
+set and removing that number and its multiples from the set.
+The player that cannot make a move loses the game.
 They play x rounds of the game, where n may be different for each round.
 """
+
+
+def isPrime(num):
+    # Return 'True' if n is a prime number. False otherwise
+    # Eliminates multiples of 2 and 3
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while 1 * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+        return True
 
 
 def isWinner(x, nums):
@@ -16,39 +32,32 @@ def isWinner(x, nums):
     You can assume n and x will not be larger than 10000
     You cannot import any packages in this task
     """
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * 1 <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
+    def canWin(n):
+        # Create a list to store the results of the subproblems
+        dp = [False] * (n + 1)
 
-    def play_round(win):
-        maria_turn = True
-        while num > 0:
-            if is_prime(num):
-                return "Maria" if maria_turn else "Ben"
-            for i in range(num, 0, -1):
-                if is_prime(i):
-                    num -= i
-                    maria_turn = not maria_turn
+        # Base case: Ben wins when there are no prime numbers left
+        dp[0] = False
+
+        # Check all numbers from 1 to n
+        for i in range(2, n + 1):
+            # If the curent number is prime and there exists a move
+            # such that Maria loses, then Ben wins
+            for j in range(2, i + 1):
+                if isPrime(j) and i % j == 0 and not dp[i - j]:
+                    dp[i] = True
                     break
 
+        return dp[n]
+
+    # Initialize counters to keep track of wins
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        winner = play_round(n)
-        if winner == "Maria":
+        if canWin(n):
             maria_wins += 1
-        elif winner == "Ben":
+        else:
             ben_wins += 1
 
     if maria_wins > ben_wins:
