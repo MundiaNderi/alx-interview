b7aa561dd950e48831e1e46129b652d01b7a9aed
MundiaNderi <sarahngima77@gmail.com>
2023-10-05 11:10:14 +0300

    Fixing pycodestyle

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Given a set of consecutive integers starting from 1 up to and including n, 
-they take turns choosing a prime number from the set and removing that number and 
-its multiples from the set. The player that cannot make a move loses the game.
-They play x rounds of the game, where n may be different for each round. 
+Given a set of consecutive integers starting from 1 up to and including n,
+they take turns choosing a prime number from the set and removing that
+number and its multiples from the set. The player that
+cannot make a move loses the game.
+They play x rounds of the game, where n may be different for each round.
 """
 
 
